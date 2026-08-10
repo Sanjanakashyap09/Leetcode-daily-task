4a7b7859bacac5af153ef3d5198e04a598c9aa34
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-08-10 09:13:29 +0530

    Time: 7 ms (77.22%) | Memory: 13.7 MB (55.98%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> memo;
+
+    int dfs(int i, int M) {
+        if (i >= n) return 0;
+        if (memo[i][M] != -1) return memo[i][M];
+
+        int best = 0;
+        for (int X = 1; X <= 2*M && i+X <= n; X++) {
+            best = max(best, suffix[i] - dfs(i+X, max(M, X)));
+        }
+        return memo[i][M] = best;
+    }
+
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();        //len of piles
-        vector<int>suffix(n+1, 0);    //total stones from i to end
+        n = piles.size();
+        suffix.assign(n+1, 0);
+        memo.assign(n+1, vector<int>(n+1, -1));
 
-        for(int i=n-1; i>=0; i--){
-            suffix[i]= suffix[i+1]+piles[i];    // fill suffix sum array from right to left
+        for (int i = n-1; i >= 0; i--) {
+            suffix[i] = suffix[i+1] + piles[i];
         }
-        vector<vector<int>>dp(n+1, vector<int>(n+1,0));    //create dp table
 
-        for(int i=n-1; i>=0; i--){        //outer loop to solve subproblem first
-            for(int M=1; M<=n; M++){      //middle loop to chcek every possible value of M
-                for(int X=1; X<=2*M && i+X<= n; X++){      //inner loop for Alice(total count of piles taken by Alice)
-                    dp[i][M]= max(dp[i][M], suffix[i]- dp[i+X][max(M,X)]);
-                }
-            }
-        }
-        return dp[0][1];        //final max stone choosen by Alice
-        
+        return dfs(0, 1);
     }
 };
