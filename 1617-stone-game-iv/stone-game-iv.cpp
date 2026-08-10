d68f5607bd7a37797889b935d4d1e02bdcc4468e
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-08-10 18:09:21 +0530

    Time: 41 ms (63.18%) | Memory: 16.4 MB (46.98%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    vector<int> dp;
+    vector<int> dp;         //dp array
     bool helper(int n) {
-        if (n == 0)
+
+         //base case---if stones end teh player will loose the game     
+        if (n == 0)        
             return false;
+        //in case of already choosen tehn simply retrun it    
         if (dp[n] != -1)
             return dp[n];
 
-        for (int i = 1; i * i <= n; i++) {
-            if (!helper(n - i * i))
+        for (int i = 1; i * i <= n; i++) {    //try to remove every possible perfect sqr
+            if (!helper(n - i * i))           //if opponent get loose then current will definitely won
                 return dp[n] = true;
         }
-        return dp[n] = false;
+        return dp[n] = false;           //current one will loose the game if not any winning move left
     }
     bool winnerSquareGame(int n) {
 
