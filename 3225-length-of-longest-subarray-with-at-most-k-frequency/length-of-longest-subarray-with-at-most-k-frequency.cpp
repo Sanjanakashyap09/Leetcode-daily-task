58fe9d188da80a70991641669189ffeed4b5ae16
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-08-12 19:37:08 +0530

    Time: 63 ms (63.26%) | Memory: 149.4 MB (56.74%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
-        unordered_map<int, int>freq;     //hash map to store freq count
-
+        unordered_map<int, int>freq;
         int n = nums.size();
-        
-        int left=0;         //start from left
-        int ans = 0;        //longest subarray len till now
+        int left = 0; // left ptr to start from left always
+        int ans = 0;  // initialize sum is 0
 
-        for(int right=0; right<nums.size(); right++){    //for loop to traverse each element or thier upcoming freq.
-            freq[nums[right]]++;         //update the freq. count in map
+        for (int right = 0; right < nums.size(); right++) { // check from right side or updtae their freq. at each step
+            freq[nums[right]]++;
 
-            while(freq[nums[right]]>k){        //when count of freq. exceed the kth limit then reduce the left size or update under the map
-                freq[nums[left]]--;         
+            while (freq[nums[right]] > k) {
+                freq[nums[left]]--;
                 left++;
             }
-
-            ans = max(ans, right-left+1);   //update longest valid subarray length
-
+            ans = max(ans, right - left + 1);
         }
-        return ans;     //final count
-        
+        return ans;
+       
     }
 };
