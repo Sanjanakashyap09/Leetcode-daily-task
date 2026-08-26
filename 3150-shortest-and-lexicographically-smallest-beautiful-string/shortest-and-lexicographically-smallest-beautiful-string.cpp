6362a1c5bfe12b29a751e48d61297efba5f2b9ba
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-08-26 12:17:55 +0530

    Time: 7 ms (12.09%) | Memory: 11.4 MB (5.79%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.length();
+        for (int len = k; len <= n; len++) {   // check all lengths >= k
+            string result = "";
+
+            for (int start = 0; start <= n - len; start++) {
+                string temp = s.substr(start, len);
+
+                int ones = 0;
+                for (char &ch : temp) {
+                    ones += (ch == '1') ? 1 : 0;
+                }
+
+                if (ones == k) {
+                    if (result.empty() || temp < result) {
+                        result = temp;
+                    }
+                }
+            }
+            if (!result.empty()) {
+                return result;   // return first valid shortest substring
+            }
+        }
+        return ""; // if no substring found
+    }
+};
