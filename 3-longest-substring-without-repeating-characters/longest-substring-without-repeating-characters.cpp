06e243e8f387ff3200bd708c6a08ec6c7b0c7072
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-09-08 11:05:33 +0530

    Time: 306 ms (16.64%) | Memory: 81.3 MB (21.17%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> st;
+        unordered_set<char> st;  //create set to store unique elem.
 
         int left = 0; // as left pointer
-        int ans = 0;  // store max len.
+        int ans = 0;  // to store max len. found
 
-        // remove char. until current one's becomes unique
+        //remove char. until current one's becomes unique
         for (int right = 0; right < s.length(); right++) {
-            while (st.find(s[right]) != st.end()) {
-                st.erase(s[left]);
-                left++;
+            while (st.find(s[right]) != st.end()) {   //for dupe's check
+                st.erase(s[left]);    //to remove dupe's
+                left++;   
             }
             //add current char to the window
             st.insert(s[right]);
