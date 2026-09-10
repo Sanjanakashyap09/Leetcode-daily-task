2c211aa99dd5f968287d078c628a4a5678af791c
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-09-10 21:14:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (64.72%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -13,27 +13,25 @@
 class Solution {
 public:
     int result;
-    pair<int, int>solve(TreeNode* root){
-        if(!root){
-            return {0,0};
+    pair<int, int> solve(TreeNode* root) {
+        if (!root) {
+            return {0, 0};
         }
-        auto P1= solve(root->left);
-        auto P2= solve(root->right);
+        auto P1 = solve(root->left);
+        auto P2 = solve(root->right);
 
-        int totalSum = P1.first+P2.first+ root->val;
-        int totalCount = P1.second+P2.second+1;
+        int totalSum = P1.first + P2.first + root->val;
+        int totalCount = P1.second + P2.second + 1;
 
-        int avg=totalSum/totalCount;
+        int avg = totalSum / totalCount;
 
-        if(avg==root->val){
-            result +=1;
-
+        if (avg == root->val) {
+            result += 1;
         }
-        return{totalSum, totalCount};
-                
+        return {totalSum, totalCount};
     }
     int averageOfSubtree(TreeNode* root) {
-        result=0;
+        result = 0;
         solve(root);
         return result;
     }
