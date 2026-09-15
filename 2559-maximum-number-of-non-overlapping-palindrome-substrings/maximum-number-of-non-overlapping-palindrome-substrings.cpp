119d8ce471beef44612c100588c98e9164338986
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-09-15 22:47:48 +0530

    Time: 281 ms (21.20%) | Memory: 19.7 MB (34.82%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    vector<int> t;
+
+    int solve(string& s, int len, int k) {
+        if (len < k)
+            return 0;
+
+        if (t[len] != -1)
+            return t[len];
+
+        int result = solve(s, len - 1, k);
+
+        int j = len - 1;
+
+        for (int i = 0; j - i + 1 >= k; i++) {
+            if (isPalindrome[i][j]) {
+                result = max(result, 1 + solve(s, i, k));
+            }
+        }
+
+        return t[len] = result;
+    }
+
+    int maxPalindromes(string s, int k) {
+        int n = s.length();
+
+        isPalindrome.assign(n, vector<bool>(n + 1, false));
+
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                }
+                else if (i + 1 == j) {
+                    isPalindrome[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    isPalindrome[i][j] =
+                        (s[i] == s[j]) &&
+                        isPalindrome[i + 1][j - 1];
+                }
+            }
+        }
+
+        t.assign(n + 1, -1);
+
+        return solve(s, n, k);
+    }
+};
+
