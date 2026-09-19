b623a8a23228b67a84ec89543a2a38f529eeab55
Sanjana <141105798+Sanjanakashyap09@users.noreply.github.com>
2026-09-19 19:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.38%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xi= clamp(xCenter, x1, x2); // Find the closest x-coordinate of the rectangle to the circle's center
+        int yi= clamp(yCenter, y1,y2); //same as x
+
+        if(y1>yCenter){
+            yi= y1;
+        }
+        else if(y2<yCenter){
+            yi= y2;
+        }
+        else{
+            yi= yCenter;
+        }
+
+             // We compare squared distances to avoid using sqrt()
+        return sqrt((xi-xCenter)* (xi-xCenter)+ (yi- yCenter)* (yi- yCenter))<= radius;
+        
+    }
+};
